54d9dff34d3cb64aab9fa3407541f9c731009ab0
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-19 17:09:47 +0100

    Created routes and views for admins

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,18 @@ use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
+
+    Route::get('/', AdminHomeController::class)->middleware(['auth', 'verified'])->name('home');
+
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projects{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
